1bfa1fca9242daa22828cab6c536ca192bc8c9b2
normal <specialnormal@naver.com>
2024-01-23 14:21:23 +0900

    day01 v0.2 변수

--- test.cpp
+++ test.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "hello world" "C++ 익히자" << std::endl;
+    int num1;
+    int num2;
+    int sum;
+
+    cout << "첫번째 숫자 입력: ";
+    cin >> num1;
+    cout << "두번째 숫자 입력: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "두 숫자의 합: " << sum;
+    
     return 0;
+
 }
