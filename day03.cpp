dc0c99199354069ff95043f8010c5d22f81efac9
normal <specialnormal@naver.com>
2024-01-25 15:41:53 +0900

    day03 v 1.2 reference

--- day03.cpp
+++ day03.cpp
@@ -3,42 +3,21 @@
 #include <iomanip>
 using namespace std;
 
+void fun (int y);
 
 int main()
 {
-    // //6-3
-    // const double PI = 3.14159265389793238462;
-    // double degree = PI / 4;
+    int x = 10;
+    fun(x);
+    cout << "main 함수 내부의 x = "<< x << endl;
 
-    // cout << "sin(45): " << sin(degree) << endl;
-    // cout << "cos(45): " << cos(degree) << endl;
-    // cout << "tan(45): " << tan(degree) << endl;
-
-    //6-4
-    const double PI = 3.14159265389793238462;
-    int n =0;
-    double s, peri, area;
-
-    do
-    {
-        cout << "변의 개수를 입력하세요(4이상의 정수): ";
-        cin >> n;
-    } while (n < 4);
-
-    do
-    {
-        cout << "변의 길이를 입력하세요: ";
-        cin >> s;
-    } while (s < 0.0);
-
-    peri = n * s;
-    area = (n * pow(s,2)) / (n * tan(PI / n));
-
-    cout << "둘레: " << peri << endl;
-    cout << "넓이: " << area << endl;
-
-    
-    
     return 0;
     
+}
+
+void fun (int y)
+{
+    y++;
+    cout << "fun 함수 내부의 y = " << y << endl;
+    return;
 }
