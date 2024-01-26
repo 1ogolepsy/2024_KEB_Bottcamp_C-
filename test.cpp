88522f41fcaa24959a4445167c2a7d7ee7507b4c
normal <specialnormal@naver.com>
2024-01-26 11:38:46 +0900

    day04 v1.3 class

--- test.cpp
+++ test.cpp
@@ -2,10 +2,31 @@
 #include <string>
 using namespace std;
 
+// 클래스 안에서 함수 정의하기
+class Circle
+{
+    private:
+        double radius;
+    public:
+        double getRadius () const
+        {
+            return radius;
+        }
+        void setRadius (double value);
+
+};
+
+void Circle::setRadius(double value)
+{
+    radius = value;
+}
+
 int main()
 {
-    char test = 'a';
-    cout << static_cast<bool>(test);
+    Circle circle1;
+    circle1.setRadius(10.0);
+    cout << circle1.getRadius() << endl;
+    
 
     return 0;
 }
