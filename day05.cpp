b26d0ee9adf718c4f65f3f7223816b8b4972ff08
normal <specialnormal@naver.com>
2024-01-29 18:58:27 +0900

    day05 v1.6 array end

---
+++ day05.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <typeinfo>
+using namespace std;
+
+int main()
+{
+    const int CAPACITY = 10;
+    int frequencies[CAPACITY] = {0};
+    ifstream integerFile;
+
+    integerFile.open("integerFile.dat");
+    if(!integerFile)
+    {
+        cout << "숫자 파일을 열 수 없습니다. " << endl;
+        cout << "프로그램을 중단합니다,";
+        return 0;
+    }
+
+    int data = 0;
+    int size = 0;
+    
+    cout << data;
+    while(integerFile.eof())
+    {
+        integerFile >> data;
+        cout << "inside" << data;
+        cout << typeid(data).name();
+        if(data >= 0 && data <= 9)
+        {
+            size++;
+            frequencies[data]++;
+        }
+    }
+    cout << data;
+    integerFile.close();
+
+    cout << "파일 안에 " << size << "개의 유효한 데이터가 있습니다. " << endl;
+
+    for(int i = 0; i < 10; i++)
+    {
+        cout << setw(3) << i << " ";
+
+        for(int f=1; f <= frequencies[i]; f++)
+        {
+            cout << '*';
+        }
+        cout << " " << frequencies[i] << endl;
+    }
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
-#include <string>
+#include <cctype>
 using namespace std;
-
-// 클래스 안에서 함수 정의하기
-class Circle
-{
-    private:
-        double radius;
-    public:
-        double getRadius () const
-        {
-            return radius;
-        }
-        void setRadius (double value);
-
-};
-
-void Circle::setRadius(double value)
-{
-    radius = value;
-}
 
 int main()
 {
-    Circle circle1;
-    circle1.setRadius(10.0);
-    cout << circle1.getRadius() << endl;
-    
-
+    int array[50] = {1,1,2,1,1,1,1,1,1,1};
+    int size = 10;
+    for(int i = 3; i < size; i++)
+    {
+        array[i-1] = array[i];
+    }
+    size--;
+    cout << array[9];
     return 0;
 }
